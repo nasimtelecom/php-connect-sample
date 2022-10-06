f628cefee05957ed03153ea27b8aa64b7e26f944
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2022-10-06 20:11:22 +0330

    Update smartApi.php

--- smartApi.php
+++ smartApi.php
@@ -5,12 +5,20 @@ require("./vendor/autoload.php");
 use NasimTelecom\Simotel\Simotel;
 use App\Log;
 
+
+$requestData = $_REQUEST;
+$config = require("config.php");
+$log = new Log;
 try{
-    $config = require("config.php");
     $simotel = new Simotel($config);
-    $res = $simotel->smartApi($_REQUEST);
+    $res = $simotel->smartApi($requestData)->toJson();
+    header('Content-Type: application/json; charset=utf-8');
     echo $res;
+
 }
 catch(Exception $ex){
-    die($ex->getMessage());
+    $errorMessage = $ex->getMessage();
+    $log->error($errorMessage);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(["error"=>$errorMessage]);
 }
